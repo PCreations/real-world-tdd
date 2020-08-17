f5e18a81da216690add48656ddf444ad87ab49f6
Pierre Criulanscy <pierre_criulanscy.external@discovery.com>
2020-08-17 16:33:19 +0200

    add generateSitemapXML logic

---
+++ src/__tests__/generate-sitemap-xml.test.js
@@ -0,0 +1,26 @@
+import { createTestArticle } from "./create-test-article";
+import { createArticleToXML } from "../article-to-xml";
+import { generateSitemapXML } from "../generate-sitemap-xml";
+
+describe("generateSitemapXml", () => {
+  it("generates the sitemap xml for the given language and articles", () => {
+    // arrange
+    const language = "en-GB";
+    const articles = [
+      createTestArticle(),
+      createTestArticle(),
+      createTestArticle(),
+    ];
+    const articleToXML = createArticleToXML(language);
+
+    // act
+    const xml = generateSitemapXML({ language, articles });
+
+    // assert
+    expect(xml).toEqual(
+      `<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?><urlset xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\" xmlns:news=\\"http://www.google.com/schemas/sitemap-news/0.9\\">${articles
+        .map(articleToXML)
+        .join("")}</urlset>`
+    );
+  });
+});
---
+++ src/generate-sitemap-xml.js
@@ -0,0 +1,8 @@
+import { createArticleToXML } from "./article-to-xml";
+
+export const generateSitemapXML = ({ language, articles }) => {
+  const articleToXML = createArticleToXML(language);
+  return `<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?><urlset xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\" xmlns:news=\\"http://www.google.com/schemas/sitemap-news/0.9\\">${articles
+    .map(articleToXML)
+    .join("")}</urlset>`;
+};
