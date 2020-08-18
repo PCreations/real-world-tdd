fafadde97aaf29fa3f6fbdbb99cf4b29910e6a79
Pierre Criulanscy <pierre_criulanscy.external@discovery.com>
2020-08-18 15:39:41 +0200

    fix bad xml formatting

--- src/__tests__/generate-sitemap-xml.test.js
+++ src/__tests__/generate-sitemap-xml.test.js
@@ -18,7 +18,7 @@ describe("generateSitemapXml", () => {
 
     // assert
     expect(xml).toEqual(
-      `<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?><urlset xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\" xmlns:news=\\"http://www.google.com/schemas/sitemap-news/0.9\\">${articles
+      `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">${articles
         .map(articleToXML)
         .join("")}</urlset>`
     );
