dbe9b5e4d03b4fe2045d99479713019e95504d91
Pierre Criulanscy <pierre_criulanscy.external@discovery.com>
2020-08-18 15:58:14 +0200

    add embeded stubs for xml uploader

--- src/__tests__/xml-uploader.test.js
+++ src/__tests__/xml-uploader.test.js
@@ -23,4 +23,48 @@ describe("xmlUploader", () => {
     // assert
     expect(xmlUploader.getLastSentXML(domain)).toEqual(xml);
   });
+  it("can be faked", async () => {
+    // arrange
+    const domain = "www.some-url.com";
+    const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+  <loc>www.some-url.com/article-1</loc>
+  <news:news>
+    <news:publication>
+      <news:name>My Website</news:name>
+      <news:language>en-GB</news:language>
+    </news:publication>
+    <news:publication_date>2020-08-17T13:55:19.991Z</news:publication_date>
+    <news:title>article title</news:title>
+  </news:news>
+</urlset>`;
+    const xmlUploader = createFakeXMLUploader();
+
+    // act
+    await xmlUploader.upload({ domain, xml });
+
+    // assert
+    expect(xmlUploader.getLastSentXML(domain)).toEqual(xml);
+  });
+
+  it("throws an XMLUploadError if the upload fails", async () => {
+    // arrange
+    const domain = "www.some-url.com";
+    const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+  <loc>www.some-url.com/article-1</loc>
+  <news:news>
+    <news:publication>
+      <news:name>My Website</news:name>
+      <news:language>en-GB</news:language>
+    </news:publication>
+    <news:publication_date>2020-08-17T13:55:19.991Z</news:publication_date>
+    <news:title>article title</news:title>
+  </news:news>
+</urlset>`;
+    const xmlUploader = createXMLUploaderThatWillFail("some error message");
+
+    // act & assert
+    await expect(xmlUploader.upload({ domain, xml })).rejects.toEqual(
+      new XMLUploadError("some error message")
+    );
+  });
 });
--- src/xml-uploader.js
+++ src/xml-uploader.js
@@ -1,5 +1,12 @@
 import AWS from "aws-sdk";
 
+export class XMLUploadError extends Error {
+  constructor(message) {
+    super();
+    this.message = `Error while uploading XML file : ${message}`;
+  }
+}
+
 AWS.config.update({
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
@@ -12,14 +19,18 @@ export const createXMLUploader = ({ s3 = new AWS.S3() } = {}) => {
   const sentXML = {};
   return {
     async upload({ domain, xml }) {
-      await s3
-        .putObject({
-          Bucket: process.env.S3_BUCKET,
-          Key: buildKey(domain),
-          Body: xml,
-          ContentType: "application/xml",
-        })
-        .promise();
+      try {
+        await s3
+          .putObject({
+            Bucket: process.env.S3_BUCKET,
+            Key: buildKey(domain),
+            Body: xml,
+            ContentType: "application/xml",
+          })
+          .promise();
+      } catch (error) {
+        throw new XMLUploadError(error.message);
+      }
       sentXML[buildKey(domain)] = xml;
     },
     getLastSentXML(domain) {
@@ -27,3 +38,21 @@ export const createXMLUploader = ({ s3 = new AWS.S3() } = {}) => {
     },
   };
 };
+
+const createFakeS3 = ({ uploadErrorMessage = null } = {}) => ({
+  putObject() {
+    return {
+      promise() {
+        return uploadErrorMessage
+          ? Promise.reject(new Error(uploadErrorMessage))
+          : Promise.resolve();
+      },
+    };
+  },
+});
+
+export const createFakeXMLUploader = () =>
+  createXMLUploader({ s3: createFakeS3() });
+
+export const createXMLUploaderThatWillFail = (uploadErrorMessage) =>
+  createXMLUploader({ s3: createFakeS3({ uploadErrorMessage }) });
